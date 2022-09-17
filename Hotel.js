0eafdf5b4b02963497c31fde34c81c6fedb2aa7f
Gabriel Rodrigo <pdktche-pb@live.com>
2022-09-16 21:36:18 -0300

    Foi criado as funçoes de cadastrar hotel e reserva.

--- Hotel.js
+++ Hotel.js
@@ -2,6 +2,12 @@ let hotel = []
 let reserva = []
 let contador = 0
 
+FazerCadastro();
+CadastrarReserva();
+
+
+
+
 
 function FazerCadastro(){
     let id = contador
@@ -9,8 +15,28 @@ function FazerCadastro(){
     let categoria = prompt("Digite a categoria que deseja ficar: ")
     let endereco = prompt("Digite o endereço: ")
     let telefone = prompt("Digite o telefone: ")
-    hotel[hotel.length] = id,nome,categoria,endereco,telefone
+    hotel[hotel.length] = id
+    hotel[hotel.length] = nome
+    hotel[hotel.length] = categoria
+    hotel[hotel.length] = endereco
+    hotel[hotel.length] = telefone
     if(id == contador){
         contador++
+    }
+}
+function CadastrarReserva(){
+    let id = contador
+    let idHotel = 0
+    let nomeResponsavel = prompt("Digite o nome do responsavel: ")
+    let diaEntrada = prompt("Digite o dia de Entrada: ")
+    let diaSaida = prompt("Digite o dia da Saida: ")
+    reserva[reserva.length] = id
+    reserva[reserva.length] = idHotel
+    reserva[reserva.length] = nomeResponsavel
+    reserva[reserva.length] = diaEntrada
+    reserva[reserva.length] = diaSaida
+    if(id == contador){
+        contador++
+        idHotel++
     }
 }
